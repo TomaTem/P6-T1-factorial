af38a16683164dc340ccb57a5988b772721946e9
tomatem <tem7720@gmail.com>
2023-04-17 23:54:28 +0300

    done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // Итерационный подсчет факториала
-function factIterat(num){
-
+function factIterat(num) {
+    let result = 1;
+    while (num !== 1) {
+        result *= num;
+        num = num - 1;
+    }
+    return result;
 }
 
 // Рекурсивный расчет факториала
-function factRec(num){
- 
+function factRec(num) {
+    if (num == 1) {
+        return 1;
+    } else {
+        return num * factRec(num - 1);
+    }
 }
+
+console.log(factRec(10));
+console.log(factIterat(10));
